91b217c77f3b758b155af216c258c209040b2e9d
Glayson <glaysonbss@gmail.com>
2022-05-25 18:13:40 -0300

    Multiple changes: v2.0 of churrascometro app uploaded

--- js/script.js
+++ js/script.js
@@ -3,65 +3,46 @@
 // Refrigerante/água - 1000ml por pessoa. + de 6h = 1500ml
 // Criança vale 0,5 adulto.
 
-function qtdPessoas() {
-  let adultos = document.getElementById("adulto");
-  let numAdultos = parseInt(adultos.value);
-  let criancas = document.getElementById("crianca");
-  let numCriancas = parseInt(criancas.value);
+let inputAdulto = document.getElementById('adulto');
+let inputCrianca = document.getElementById('crianca');
+let inputDuracao = document.getElementById('duracao');
 
-  isNaN(numAdultos) ? (numAdultos = 0) : numAdultos;
-  isNaN(numCriancas) ? (numCriancas = 0) : numCriancas;
+let resultado = document.getElementById('resultado');
 
-  let qtdTotal = numAdultos + numCriancas / 2;
-  return qtdTotal;
+function calcular() {
+  let adulto = inputAdulto.value;
+  let crianca = inputCrianca.value;
+  let duracao = inputDuracao.value;
+
+  let qtdTotalCarne = carnePP(duracao) * adulto + (400 / 2 * crianca);
+  let qtdTotalCerveja = carnePP(duracao) * adulto;
+  let qtdTotalBebida = carnePP(duracao) * adulto + (400 / 2 * crianca);
+
+  resultado.innerHTML = `<p>${qtdTotalCarne / 1000} Kg de Carne</p>`
+  resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 350)} Latas de Cerveja</p>`
+  resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebida / 2000)} Garrafas de 2L de Bebidas</p>`
 }
 
-function calculo() {
-  let myDiv = document.getElementsByTagName("div")[0];
-  let duracao = document.getElementById("duracao");
-  let valorDuracao = duracao.value;
-
-  isNaN(valorDuracao) ? (valorDuracao = 0) : valorDuracao;
-
-  let qtdCarne = 0.4;
-  let qtdCerveja = 1.2;
-  let qtdBebida = 1;
-
-  if (valorDuracao <= 4) {
-    let qtdCarneTotal = qtdCarne * qtdPessoas();
-    let qtdCervejaTotal = qtdCerveja * qtdPessoas();
-    let qtdBebidaTotal = qtdBebida * qtdPessoas();
-
-    let textCarne = document.createElement("p");
-    textCarne.innerHTML = qtdCarneTotal.toFixed(1) + " Kg de Carne";
-    myDiv.appendChild(textCarne);
-
-    let textCerveja = document.createElement("p");
-    textCerveja.innerHTML = qtdCervejaTotal.toFixed(1) + " L de Cerveja";
-    myDiv.appendChild(textCerveja);
-
-    let textBebida = document.createElement("p");
-    textBebida.innerHTML = qtdBebidaTotal.toFixed(1) + " L de Bebida";
-    myDiv.appendChild(textBebida);
+function carnePP (duracao) {
+  if (duracao >=6) {
+    return 650;
   } else {
-    qtdCarne = 0.65;
-    qtdCerveja = 2;
-    qtdBebida = 1.5;
+    return 400;
+  }
+}
 
-    qtdCarneTotal = qtdCarne * qtdPessoas();
-    qtdCervejaTotal = qtdCerveja * qtdPessoas();
-    qtdBebidaTotal = qtdBebida * qtdPessoas();
+function cervejaPP (duracao) {
+  if (duracao >=6) {
+    return 2000;
+  } else {
+    return 1200;
+  }
+}
 
-    let textCarne = document.createElement("p");
-    textCarne.innerHTML = qtdCarneTotal.toFixed(1) + " Kg de Carne";
-    myDiv.appendChild(textCarne);
-
-    let textCerveja = document.createElement("p");
-    textCerveja.innerHTML = qtdCervejaTotal.toFixed(1) + " L de Cerveja";
-    myDiv.appendChild(textCerveja);
-
-    let textBebida = document.createElement("p");
-    textBebida.innerHTML = qtdBebidaTotal.toFixed(1) + " L de Bebida";
-    myDiv.appendChild(textBebida);
+function bebidaPP (duracao) {
+  if (duracao >=6) {
+    return 1500;
+  } else {
+    return 1000;
   }
 }
